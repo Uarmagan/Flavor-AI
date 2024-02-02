99c1b6184ef45117a3194ee1255f84b23cc50302
Ugur Armagan <ugurarmagan93@gmail.com>
2024-02-02 12:15:57 -0800

    adds redirect on root route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,20 +1,6 @@
-import { MetaFunction } from '@remix-run/node';
-import { Link } from '@remix-run/react';
+import { LoaderFunction, redirect } from '@remix-run/node';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: 'New Remix App' },
-    { name: 'description', content: 'Welcome to Remix!' },
-  ];
+// make a loader that redirects to the scraper route
+export const loader: LoaderFunction = async () => {
+  return redirect('/scraper');
 };
-
-export default function Index() {
-  return (
-    <Link
-      to='/scraper'
-      className=' bg-gray-700 rounded-xl text-white px-5 py-3 tracking-wide font-semibold text-sm hover:bg-gray-600 transition-colors duration-200'
-    >
-      Recipe scraper
-    </Link>
-  );
-}
